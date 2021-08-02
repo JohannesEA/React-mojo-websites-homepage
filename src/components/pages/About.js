f260890c3ec284ca6f4645cedeab5aaf5b59d225
johannesEA <johannes.erdahl.andresen@gmail.com>
2021-08-02 15:21:13 +0200

    add styling to about us seciton

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,12 +6,14 @@ const About = () => {
 
       
           <div className="about-content">   
-           <div className="about-title title">Mojo Websites</div>
-          <div className="about-text"></div>
+           <div className="about-title title">Om oss</div>
+          <div className="about-text text">Dette er et startup prosjekt som går ut på å lage enkle og svært fungerende hjemmesider for de som ønsker det. Vi er 2 studenter, en datingeniør- og en frontend student.</div>
+          <div className="about-btn"><button className="btn about-btn">Potfølje</button></div>
           </div>
 
-          <div className="about-img">
-          <img className="img"  alt="hero-img"></img>
+          <div className="about-imgs">
+          <div className="about-img about-johannes"><img src="/assets/images/headshot-johannes.jpg"  alt="johannes-img"></img>Johannes Erdahl Andresen</div>
+          <div className="about-img about-johannes"><img src="/assets/images/magnus-heashot.png"  alt="magnus-img"></img>Magnus Sødergren</div>
           </div>
        
       </div>
---
+++ src/data/data.js
@@ -0,0 +1,92 @@
+export const NORWEGIAN_NAV_TEXT = [
+
+    {
+        id: 1,
+        text: "Hjem"
+    },
+    
+    {
+        id: 2,
+        text: "Om oss"
+    },
+    {
+        id: 3,
+        text: "Service"
+    },
+    
+    {
+        id: 4,
+        text: "Portfølje"
+    },
+    {
+        id: 5,
+        text: "Kontakt oss"
+    },
+    
+];
+
+export const ENGLISH_NAV_TEXT = [
+
+    {
+        id: 1,
+        text: "Home"
+    },
+    
+    {
+        id: 2,
+        text: "About us"
+    },
+    {
+        id: 3,
+        text: "Service"
+    },
+    
+    {
+        id: 4,
+        text: "Portfolio"
+    },
+    {
+        id: 5,
+        text: "Contact us"
+    },
+];
+
+export const NORWEGIAN_PAGE_TEXT = [
+
+{
+    id: 1,
+    text: "MoJo Websites for deg som ønsker en enkel og perfekt fungerende nettside."
+},
+
+{
+    id: 2,
+    text: "Kontakt oss"
+},
+
+{
+    id: 3,
+    text: "Service"
+},
+
+
+];
+
+export const ENGLISH_PAGE_TEXT = [
+
+    {
+        id: 1,
+        text: "MoJo websites for you who want a simple and perfectly functioning website"
+    },
+    
+    {
+        id: 2,
+        text: "Contact Us"
+    },
+    
+    {
+        id: 3,
+        text: "Service"
+    },
+    
+    
+    ]
