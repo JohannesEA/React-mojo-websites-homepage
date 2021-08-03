604d4d062d218dd88a969c41aaa2fdd0b89b6c31
johannesEA <johannes.erdahl.andresen@gmail.com>
2021-08-03 14:24:54 +0200

    Add styling to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,16 +2,19 @@ import "./footer.css";
 
 const Footer = () => {
     return (
-      <div className="footer">
+      <div className="section footer">
 
       
           <div className="footer-content">   
            <div className="footer-title title">MoJo</div>
-          <div className="footer-text"></div>
+          <div className="text footer-text">MoJo Websites for deg som ønsker en enkel og perfekt fungerende nettside.</div>
+          <div className="footer-icons">
+          <i className="icon fab fa-facebook"></i>
+          <i className="icon fab fa-linkedin"></i>
+          <i className="icon fab fa-instagram"></i>
           </div>
-
-        
-       
+          </div>
+          <div className="text footer-copyright">MoJo Websites © 2021</div>
       </div>
     );
   }
