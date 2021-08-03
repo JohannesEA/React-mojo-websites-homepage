6d96df1e26863a9ecbb2e08fe22b971a216dced2
johannesEA <johannes.erdahl.andresen@gmail.com>
2021-08-03 17:24:39 +0200

    hello world

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { IMAGES } from "./data/data";
 
 
 import "./index.css"
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Portfolio from "./components/pages/Portfolio";
 
 function App() {
@@ -20,9 +20,25 @@ function App() {
   const [isNorwegian, setLanguage] = useState(false);
 
   const handleClick = () => setLanguage(!isNorwegian);
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+  }, [])
+
   return (
     <div className="App">
-  
+      {loading ? (
+      <div
+      Loader
+      className="loading-logo"
+      size={50} ><img src="/mojo-logo2.png" alt="loading-img"></img></div>
+    ) : (
+      <div className="onepage">
     <NavBar/>
     <Fab className="language-selector" color="primary" aria-label="add">
     <img  src={!isNorwegian ? "/assets/images/norwegian.png" : "/assets/images/english-logo.png"} alt="flag-img" onClick={handleClick}></img>
@@ -33,6 +49,8 @@ function App() {
     <Portfolio images={IMAGES} slidesToShow={3}/>
     <Contact/> 
      <Footer/>
+     </div>
+    )}
     </div>
   );
 }
