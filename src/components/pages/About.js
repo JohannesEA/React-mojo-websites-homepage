e7db0bd3b84ada42d4635f3555b0841c4964ddb7
johannesEA <johannes.erdahl.andresen@gmail.com>
2021-08-03 23:59:47 +0200

    Add correct link to about button

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,7 +10,7 @@ const About = () => {
           <div className="about-text text">Dette er et startup prosjekt som går ut på å lage enkle og svært fungerende hjemmesider for de som ønsker det. Vi er 2 studenter, en datingeniør- og en frontend student.</div>
           <div className="about-btn">
           <Link
-            to="contact"
+            to="portfolio"
             spy={true}
             smooth={true}
             offset={-100}
